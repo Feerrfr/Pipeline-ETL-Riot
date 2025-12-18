5aad45b492157f569ec566747dba2b0d3209ae8c
Feerrfr <fernando.franco.0.5.9@gmail.com>
2025-12-18 05:29:49 -0300

    Fondo y fix de tamanio

--- main.py
+++ main.py
@@ -22,6 +22,24 @@ def ejecutar_script(nombre_script):
     else:
         print(f"❌ Error: {nombre_script} falló. Deteniendo el pipeline.")
         return False
+def poner_fondo(url_imagen):
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("{url_imagen}");
+            background-attachment: fixed;
+            background-size: cover;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+url_fondo = "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/4QFZRNIDT5BVNNGLOUZ4SHORCE.jpg"
+poner_fondo(url_fondo)
+
+st.set_page_config(page_title="LoL Data Lake", page_icon="⚔️", layout="wide")
 
 # --- EL FLUJO DEL PIPELINE ---
 if __name__ == "__main__":
@@ -29,7 +47,9 @@ if __name__ == "__main__":
 
     st.title("🚀 Analisis a un jugador de League Of Legends ETL:")
     st.write("Panel de control")
-    
+    st.dataframe(transformacion.transformar_partidas(transformacion.entregar_bronze()))
+
+
     if st.button("🚀 Correr script completo"):# 1. Ejecutar Bronze (Extracción)
         extraccion.extraccion_lolstats()
         print("✅ Extracción completada.\n")
